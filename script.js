dac2023860c91d1ae6ae7fed9a49110a772b5265
Andrey Getmanenko <144361608+Andron1k@users.noreply.github.com>
2023-10-10 01:42:51 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,59 @@
+const canvas = document.getElementById('canvas');
+const ctx = canvas.getContext('2d');
+
+let painting = false;
+
+canvas.width = 1000;
+canvas.height = 600;
+
+ctx.lineWidth = 5;
+ctx.lineCap = 'round';
+ctx.strokeStyle = "black" // Початковий колір пензля
+
+function startPosition(e) {
+    painting = true;
+    draw(e);
+}
+
+function endPosition() {
+    painting = false;
+    ctx.beginPath();
+}
+
+function draw(e) {
+    if (!painting) return;
+
+    const rect = canvas.getBoundingClientRect();
+    const x = e.clientX - rect.left;
+    const y = e.clientY - rect.top;
+
+    ctx.lineTo(x, y);
+    ctx.stroke();
+    ctx.beginPath();
+    ctx.moveTo(x, y);
+}
+
+canvas.addEventListener('mousedown', startPosition);
+canvas.addEventListener('mouseup', endPosition);
+canvas.addEventListener('mousemove', draw);
+canvas.addEventListener('mouseleave', endPosition);
+
+
+
+// Очищення
+const resetButton = document.getElementById('resetButton');
+resetButton.addEventListener('click', resetCanvas);
+
+
+
+
+// Додавання кольору
+const colorPicker = document.getElementById('colorPicker');
+function setColor() {
+    ctx.strokeStyle = colorPicker.value;
+}
+colorPicker.addEventListener('input', setColor);
+
+
+
+
